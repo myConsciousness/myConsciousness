4cadd0c76218ab17d9610857fb89e55f84b9fb08
myConsciousness <contact@shinyakato.dev>
2023-04-07 09:46:41 +0900

    fix: upgrade

--- bin/auto_update.dart
+++ bin/auto_update.dart
@@ -6,7 +6,6 @@ import 'package:html/parser.dart';
 import 'package:http/http.dart';
 import 'package:intl/intl.dart';
 import 'package:nasa/nasa.dart';
-import 'package:twitter_api_v2/twitter_api_v2.dart' as v2;
 
 const _myRankInGitHubSectionStart =
     '<!-- MY-RANK-IN-GITHUB:START - Do not remove or modify this section -->';
@@ -15,10 +14,6 @@ const _myRankInGitHubSectionEnd = '<!-- MY-RANK-IN-GITHUB:END -->';
 const _myBskyTimelineSectionStart =
     '<!-- MY-BSKY_TIMELINE:START - Do not remove or modify this section -->';
 const _myBskyTimelineSectionEnd = '<!-- MY-BSKY_TIMELINE:END -->';
-
-const _myTweetSectionStart =
-    '<!-- MY-TWEETS:START - Do not remove or modify this section -->';
-const _myTweetSectionEnd = '<!-- MY-TWEETS:END -->';
 
 const _myZennArticlesSectionStart =
     '<!-- MY-ZENN-ARTICLES:START - Do not remove or modify this section -->';
@@ -31,14 +26,13 @@ const _apodSectionEnd = '<!-- APOD:END -->';
 Future<void> main(List<String> arguments) async {
   await _updateGitHubRanking();
   await _updateBlueskyTimeline();
-  await _updateTweets();
   await _updateAPOD();
   await _updateZennArticles();
 }
 
 Future<void> _updateBlueskyTimeline() async {
   final session = await bsky.createSession(
-    handle: Platform.environment['BLUESKY_HANDLE']!,
+    identifier: Platform.environment['BLUESKY_IDENTIFIER']!,
     password: Platform.environment['BLUESKY_PASSWORD']!,
   );
 
@@ -53,13 +47,13 @@ Future<void> _updateBlueskyTimeline() async {
     ),
   );
 
-  final feeds = await bluesky.feeds.getFeeds(
-    author: 'shinyakato.dev',
+  final feeds = await bluesky.feeds.findFeed(
+    actor: 'shinyakato.dev',
     limit: 5,
   );
 
   final postUIs = <String>[];
-  for (final feed in feeds.data.feeds) {
+  for (final feed in feeds.data.feed) {
     final post = feed.post;
     final me = post.author;
 
@@ -95,71 +89,6 @@ Future<void> _updateGitHubRanking() async {
       '''\n\n🤖 **Fun fact 1**: I'm currently [the ${await _getRankAsGitHubCommitter()} most active GitHub committer in Japan](https://commits.top/japan.html).</br>
 🤖 **Fun fact 2**: I'm currently rated as [the ${await _getRankAsGitHubContributor()} most active GitHub contributor in Japan](https://commits.top/japan_public.html).</br>
 🤖 **Fun fact 3**: I'm described in [Wikipedia](https://ja.wikipedia.org/wiki/加藤真也_(プログラマ)).\n\n''',
-    ),
-  );
-}
-
-Future<void> _updateTweets() async {
-  final twitter = v2.TwitterApi(
-    bearerToken: '',
-    oauthTokens: v2.OAuthTokens(
-      consumerKey: Platform.environment['TWITTER_CONSUMER_KEY']!,
-      consumerSecret: Platform.environment['TWITTER_CONSUMER_SECRET']!,
-      accessToken: Platform.environment['TWITTER_ACCESS_TOKEN']!,
-      accessTokenSecret: Platform.environment['TWITTER_ACCESS_TOKEN_SECRET']!,
-    ),
-    retryConfig: v2.RetryConfig(
-      maxAttempts: 10,
-      onExecute: (event) => print(
-        'Retry after ${event.intervalInSeconds} seconds... '
-        '[${event.retryCount} times]',
-      ),
-    ),
-  );
-
-  final me = await twitter.users.lookupMe(
-    userFields: [
-      v2.UserField.username,
-      v2.UserField.profileImageUrl,
-    ],
-  );
-
-  final tweets = await twitter.tweets.lookupTweets(
-    userId: me.data.id,
-    maxResults: 5,
-    expansions: [
-      v2.TweetExpansion.attachmentsMediaKeys,
-    ],
-    tweetFields: [
-      v2.TweetField.createdAt,
-      v2.TweetField.attachments,
-    ],
-    mediaFields: [
-      v2.MediaField.altText,
-      v2.MediaField.url,
-    ],
-  );
-
-  final tweetUIs = <String>[];
-  for (final tweet in tweets.data) {
-    tweetUIs.add('''\n> ![${me.data.name}'s avatar](${me.data.profileImageUrl})
-[${me.data.name}](https://twitter.com/${me.data.username}) [@${me.data.username}](https://twitter.com/${me.data.username}) [${tweet.createdAt!.toUtc().toIso8601String()}](https://twitter.com/${me.data.username}/status/${tweet.id})
->
-> ${_getTweetText(me.data, tweet, tweets.includes)}
->
-> [Reply](https://twitter.com/intent/tweet?in_reply_to=${tweet.id})&emsp;[Retweet](https://twitter.com/intent/retweet?tweet_id=${tweet.id})&emsp;[Like](https://twitter.com/intent/favorite?tweet_id=${tweet.id})
-''');
-  }
-
-  final readme = File('README.md');
-  String content = readme.readAsStringSync();
-
-  readme.writeAsStringSync(
-    _replaceFileContent(
-      content,
-      _myTweetSectionStart,
-      _myTweetSectionEnd,
-      '\n---\n${tweetUIs.join('\n---\n')}\n---\n',
     ),
   );
 }
@@ -273,68 +202,6 @@ String _replaceFileContent(
       content.indexOf(endSection),
       newContent,
     );
-
-String _getTweetText(
-  final v2.UserData me,
-  final v2.TweetData tweet,
-  final v2.Includes? includes,
-) {
-  final textElements = _activateUserReference(
-    _activateHashtags(tweet.text),
-  ).split('\n');
-
-  if (tweet.attachments != null) {
-    final attachments = tweet.attachments!;
-
-    if (attachments.mediaKeys != null) {
-      for (final mediaKey in attachments.mediaKeys!) {
-        for (final media in includes!.media!) {
-          if (media.key == mediaKey) {
-            if (media.url != null) {
-              textElements.add(
-                '![${media.altText ?? 'No AltText'}](${media.url})',
-              );
-            }
-          }
-        }
-      }
-    }
-  }
-
-  return textElements.join('\n> ');
-}
-
-String _activateUserReference(final String text) {
-  final activatedText = <String>[];
-
-  final elements = text.split(' ');
-  for (final element in elements) {
-    if (element.startsWith('@')) {
-      activatedText
-          .add('[$element](https://twitter.com/${element.substring(1)})');
-    } else {
-      activatedText.add(element);
-    }
-  }
-
-  return activatedText.join(' ');
-}
-
-String _activateHashtags(final String text) {
-  final activatedText = <String>[];
-
-  final elements = text.split(' ');
-  for (final element in elements) {
-    if (element.startsWith('#')) {
-      activatedText.add(
-          '[$element](https://twitter.com/hashtag/${element.substring(1)}?src=hashtag_click)');
-    } else {
-      activatedText.add(element);
-    }
-  }
-
-  return activatedText.join(' ');
-}
 
 String _getCopyright(final APODData image) =>
     image.copyright != null ? '> &copy; ${image.copyright}' : '';
